2c8e9f3bf5cce64a981889c16d09354c4a4fb2e0
FastUserBot <103589003+FastUserBot@users.noreply.github.com>
2022-04-13 20:49:06 +0400

    Update __fast.py

--- userbot/modules/__fast.py
+++ userbot/modules/__fast.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2021-2022 CyberUserBot
+# Copyright (C) 2021-2022 FastUserBot
 # This file is a part of < https://github.com/FastUserbBot/FastUserBot/ >
 # Please read the GNU General Public License v3.0 in
 # <https://github.com/FastUserbBot/FastUserBot/blob/master/LICENSE/>.
