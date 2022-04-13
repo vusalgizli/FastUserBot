1d9f69ab8567e2ecb4e147343205134110d49898
FastUserBot <103589003+FastUserBot@users.noreply.github.com>
2022-04-13 21:31:05 +0400

    Update clive.py

--- userbot/modules/clive.py
+++ userbot/modules/clive.py
@@ -1,17 +1,17 @@
-# Copyright (C) 2021-2022 CyberUserBot
-# This file is a part of < https://github.com/FaridDadashzade/CyberUserBot/ >
+# Copyright (C) 2021-2022 FastUserBot
+# This file is a part of < https://www.github.com/FastUserBot/FastUserBot/ >
 # Please read the GNU General Public License v3.0 in
-# <https://www.github.com/FaridDadashzade/CyberUserBot/blob/master/LICENSE/>.
+# <https://www.github.com/FastUserBot/FastUserBot/blob/master/LICENSE/>.
 
 import asyncio
 from telethon import events
-from userbot import SUDO_ID, SUDO_VERSION, CYBER_VERSION
+from userbot import SUDO_ID, SUDO_VERSION, FAST_VERSION
 from userbot.cmdhelp import CmdHelp
 from userbot.events import register
 
 @register(sudo=True, pattern="^.calive$")
 async def sudoers(s):
-    await s.client.send_message(s.chat_id,f"`C Y B Ξ R USERBOT\nSudo aktivdir...\nC Y B Ξ R Version: {CYBER_VERSION}\nSudo Version: {SUDO_VERSION}`")
+    await s.client.send_message(s.chat_id,f"`𝙵𝙰𝚂𝚃 USERBOT\nSudo aktivdir...\n𝙵𝙰𝚂𝚃 Version: {FAST_VERSION}\nSudo Version: {SUDO_VERSION}`")
     
 Help = CmdHelp('calive')
 Help.add_command('calive', None, 'Sudo aktiv olub olmadığını yoxlamaq üçün.')
