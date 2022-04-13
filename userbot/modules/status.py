1928d2f7f7fa0ddb5e57d836c5d99a1698159a24
FastUserBot <103589003+FastUserBot@users.noreply.github.com>
2022-04-13 22:49:21 +0400

    Update status.py

--- userbot/modules/status.py
+++ userbot/modules/status.py
@@ -1,8 +1,8 @@
-# Copyright (C) 2021-2022 CyberUserBot
-# This file is a part of < https://github.com/FaridDadashzade/CyberUserBot/ >
+# Copyright (C) 2021-2022 FastUserBot
+# This file is a part of < https://www.github.com/FastUserBot/FastUserBot/ >
 # Please read the GNU General Public License v3.0 in
-# <https://www.github.com/FaridDadashzade/CyberUserBot/blob/master/LICENSE/>.
-from userbot import CMD_HELP, ASYNC_POOL, tgbot, SUDO_ID, SPOTIFY_DC, G_DRIVE_CLIENT_ID, lastfm, YOUTUBE_API_KEY, OPEN_WEATHER_MAP_APPID, AUTO_PP, REM_BG_API_KEY, OCR_SPACE_API_KEY, PM_AUTO_BAN, BOTLOG_CHATID, CYBER_VERSION
+# <https://www.github.com/FastUserBot/FastUserBot/blob/master/LICENSE/>.
+from userbot import CMD_HELP, ASYNC_POOL, tgbot, SUDO_ID, SPOTIFY_DC, G_DRIVE_CLIENT_ID, lastfm, YOUTUBE_API_KEY, OPEN_WEATHER_MAP_APPID, AUTO_PP, REM_BG_API_KEY, OCR_SPACE_API_KEY, PM_AUTO_BAN, BOTLOG_CHATID, FAST_VERSION
 from userbot.events import register
 from telethon import version
 from platform import python_version
@@ -30,7 +30,7 @@ async def durums(event):
     await event.edit(f"""
 **Python {LANG['VERSION']}:** `{python_version()}`
 **Telethon {LANG['VERSION']}:** `{version.__version__}` 
-**C Y B Ξ R {LANG['VERSION']}:** `{CYBER_VERSION}`
+**F  A  S  T {LANG['VERSION']}:** `{FAST_VERSION}`
 
 **{LANG['PLUGIN_COUNT']}:** `{len(CMD_HELP)}`
 
