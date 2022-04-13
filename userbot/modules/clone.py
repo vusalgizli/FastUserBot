7c2c3b8678d3bc37624d08cf17dee9cbf4aecd9d
FastUserBot <103589003+FastUserBot@users.noreply.github.com>
2022-04-13 21:33:24 +0400

    Update clone.py

--- userbot/modules/clone.py
+++ userbot/modules/clone.py
@@ -1,7 +1,7 @@
-# Copyright (C) 2021-2022 CyberUserBot
-# This file is a part of < https://github.com/FaridDadashzade/CyberUserBot/ >
+# Copyright (C) 2021-2022 FastUserBot
+# This file is a part of < https://www.github.com/FastUserBot/FastUserBot/ >
 # Please read the GNU General Public License v3.0 in
-# <https://www.github.com/FaridDadashzade/CyberUserBot/blob/master/LICENSE/>.
+# <https://www.github.com/FastUserBot/FastUserBot/blob/master/LICENSE/>.
 
 import html
 import os
@@ -24,7 +24,7 @@ async def clone(event):
         reply = await event.get_reply_message()
         reply_user = await event.client.get_entity(reply.from_id)
         if reply_user.id in BRAIN_CHECKER or reply_user.id in WHITELIST:
-            await event.edit("Bağışlayın, ama bir C Y B Ξ R UserBot adminini klonlamayacağam!")
+            await event.edit(" bir 𝙵𝙰𝚂𝚃 UserBot adminini klonlamayacağam!")
             return
     reply_message = await event.get_reply_message()
     replied_user, error_i_a = await get_full_user(event)
@@ -63,7 +63,7 @@ async def clone(event):
     await event.delete()
     await event.client.send_message(
       event.chat_id,
-      "`C Y B Ξ R Userbot vasitəsilə səni oğurladım..`",
+      "`𝙵𝙰𝚂𝚃 Userbot vasitəsilə səni oğurladım..`",
       reply_to=reply_message
       )
 
@@ -87,7 +87,7 @@ async def revert(event):
         await bot(functions.account.UpdateProfileRequest(about=DEFAULT_BIO))
         await event.edit(f"`{DEFAULT_NAME}, hesabınız uğurla köhnə halına qaytarıldı!`")
     except AboutTooLongError:
-        srt_bio = "@TheCyberUserBot"
+        srt_bio = "@FastSupp"
         await bot(functions.account.UpdateProfileRequest(about=srt_bio))
         await event.edit("`Hesabınız uğurla köhnə halına qaytarıldı!\nAmma bio'nuz çox uzun olduğu üçün hazır bio-dan istifadə etdim.`")
 
@@ -150,5 +150,5 @@ async def get_full_user(event):
 Help = CmdHelp('klon')
 Help.add_command('clone', '<bir istifadəçiyə cavab olaraq>', 'Qeyd etdiyiniz istifadəçinin klonu olarsınız.')
 Help.add_command('revert', None, 'Hesabınızı əvvəlki halına qaytarar.')
-Help.add_info('@TheCyberUserBot')
+Help.add_info('@FastSupp')
 Help.add()
