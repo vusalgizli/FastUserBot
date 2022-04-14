159fb88a583a22a1d6cdbbbb1d041402192d095b
FastUserBot <103589003+FastUserBot@users.noreply.github.com>
2022-04-14 22:12:36 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 # Reponu şifrələ
 # Repoda İnstallerdə Şifrəlidi❗
-# Sikdir başqa qapıya 😃
-
-    fastrobot = 'Hkxo89dbHNmsy9G8J4nmYnma1G3b2zSjnlHhapNkJSK27Akx='
-    fastrobot2 = fastrobot.encode('sikdir')
-    fastbytes = base64.b64decode(fastrobot2)
-    fastbt = fastbytes.decode('sikdir')
+# Sikdir başqa qapıya 
 
 if __name__ == '__main__':
     from userbot import main
