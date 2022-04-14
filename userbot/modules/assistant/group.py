5a47beb167f2e1c86faf39efce167889cf815159
FastUserBot <103589003+FastUserBot@users.noreply.github.com>
2022-04-14 04:36:46 +0400

    Update group.py

--- userbot/modules/assistant/group.py
+++ userbot/modules/assistant/group.py
@@ -1,7 +1,5 @@
-# Copyright (C) 2021 CyberUserBot
-# This file is a part of < https://github.com/FaridDadashzade/CyberUserBot/ >
-# PLease read the GNU General Public License v3.0 in
-# <https://www.github.com/FaridDadashzade/CyberUserBot/blob/master/LICENSE/>.
+#nədi nə baxsan?
+#👌🖕🥲
 
 from telethon import events
 from telethon.events import *
